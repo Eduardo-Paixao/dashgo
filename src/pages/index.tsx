4d9f472946327cd6348c0905daaa1bd6f2fd912c
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2023-05-03 14:24:13 -0300

    Form de autenticação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
 import { useRouter } from "next/router";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { resolve } from "path";
 
 export default function SignIn() {
-  const router = useRouter()
+  const router = useRouter();
+
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<FieldValues> = async (values) => {
+    await new Promise((resolve) => setTimeout(() => {}, 2000));
+    console.log(values, "aqui");
+    // router.push("/dashboard")
+  };
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -14,11 +24,16 @@ export default function SignIn() {
         p={8}
         borderRadius={8}
         flexDirection={"column"}
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input label="E-mail" name="email" type="email" />
-          <Input label="Senha" name="password" type="password" />
-          <Button  colorScheme="pink" onClick={()=>router.push('/dashboard')}>
+          <Input label="E-mail" type="email" {...register("email")} />
+          <Input label="Senha" type="password" {...register("password")} />
+          <Button
+            type="submit"
+            colorScheme="pink"
+            isLoading={formState.isSubmitting}
+          >
             Entrar
           </Button>
         </Stack>
